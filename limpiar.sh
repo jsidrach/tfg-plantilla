d16d0b4f3e9fbf3c094d6f9e66e6d024a908519b
JSidrach <juan.sidrach@gmail.com>
2015-05-04 18:48:45 +0200

    Bibtex is dead... long live biblatex! :crown:

--- limpiar.sh
+++ limpiar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Borrar archivos auxiliares
-rm -f *.alg *.acr *.acn *.lof *.log *.lot *.out *.bak *.toc *.xdy *.ist *.gls *.glo *.blg *.aux *.bbl *.glg *.glsdefs
+rm -f *.alg *.acr *.acn *.lof *.log *.lot *.out *.bak *.toc *.xdy *.ist *.gls *.glo *.blg *.aux *.bbl *.glg *.glsdefs main.run.xml main-blx.bib
 cd src/ &&
 rm -f *.alg *.acr *.acn *.lof *.log *.lot *.out *.bak *.toc *.xdy *.ist *.gls *.glo *.blg *.aux *.bbl *.glg *.glsdefs &&
 cd ..
