de84d998fda700d2be3e5fbd675c346d5bd977e8
JSidrach <juan.sidrach@gmail.com>
2015-05-04 18:55:11 +0200

    Minor changes

--- limpiar.sh
+++ limpiar.sh
@@ -2,5 +2,5 @@
 # Borrar archivos auxiliares
 rm -f *.alg *.acr *.acn *.lof *.log *.lot *.out *.bak *.toc *.xdy *.ist *.gls *.glo *.blg *.aux *.bbl *.glg *.glsdefs main.run.xml main-blx.bib
 cd src/ &&
-rm -f *.alg *.acr *.acn *.lof *.log *.lot *.out *.bak *.toc *.xdy *.ist *.gls *.glo *.blg *.aux *.bbl *.glg *.glsdefs &&
+rm -f *.alg *.acr *.acn *.lof *.log *.lot *.out *.bak *.toc *.xdy *.ist *.gls *.glo *.blg *.aux *.bbl *.glg *.glsdefs  main.run.xml main-blx.bib &&
 cd ..
